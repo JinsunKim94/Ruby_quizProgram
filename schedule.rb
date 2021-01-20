94ec93baea66888891b1a2a7e997e0aaa8fcec6c
jinsunkim94 <jinsunkim94@gmail.com>
2021-01-20 23:57:44 +0900

    20200120

---
+++ schedule.rb
@@ -0,0 +1,13 @@
+def make_dolcelatte
+  puts "1. 얼음을 넣는다."
+  puts "2. 연유를 30ml 넣는다."
+  puts "3. 찬 우유를 넣는다."
+  puts "4. 에스프레소 샷을 넣는다."
+end
+
+def make_blueberry_smoothie
+  puts "1. 블루베리를 20g 넣는다."
+  puts "2. 우유 300ml 넣는다."
+  puts "3. 얼음을 넣는다."
+  puts "4. 믹서기에 간다."
+end
