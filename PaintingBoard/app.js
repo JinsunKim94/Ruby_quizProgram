740b049db43c53dd3f670a179bef1a57bf1e64da
jinsunkimdev <jinsunkim94@gmail.com>
2021-02-21 23:13:26 +0900

    add drawing pointer

--- PaintingBoard/app.js
+++ PaintingBoard/app.js
@@ -1,1 +1,30 @@
 'use strict'
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function onMouserEnter(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x,y);
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
+
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouserEnter);
+    canvas.addEventListener("mousedown", onMousedown);
+    canvas.addEventListener("mouseup", onmouseup);
+    canvas.addEventListener("mouserleave", onmouseleave);
+}
