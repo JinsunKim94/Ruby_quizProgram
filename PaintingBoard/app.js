d2764e63119d6aacc6d60062ffc0ae13ccc2d900
jinsunkimdev <jinsunkim94@gmail.com>
2021-02-22 23:50:50 +0900

    add path function

--- PaintingBoard/app.js
+++ PaintingBoard/app.js
@@ -1,5 +1,9 @@
 'use strict'
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyles = "black";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -7,24 +11,30 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouserEnter(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x,y);
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);//마우스 가는 곳으로 path를 만든다.
+    }else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting();
-}
-
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouserEnter);
-    canvas.addEventListener("mousedown", onMousedown);
-    canvas.addEventListener("mouseup", onmouseup);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouserleave", onmouseleave);
 }
