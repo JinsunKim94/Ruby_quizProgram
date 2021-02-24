9947f68b1c7dd331812c986969216ac013524af5
jinsunkimdev <jinsunkim94@gmail.com>
2021-02-24 19:39:26 +0900

    change color_test

---
+++ ShoppingMall/index.js
@@ -0,0 +1,15 @@
+'use strict';
+const hello = document.getElementById("test");
+
+const DEFAULT_COLOR = "black";
+
+function helloWorld(){
+    const helloColor = document.getElementById("test");
+    helloColor.style.color = DEFAULT_COLOR;
+}
+
+if(hello){
+    hello.addEventListener("click", helloWorld);
+}
+
+console.log(helloWorld);
