6c14bd489aecf0d151a3b3354957d963b98a915d
jinsunkimdev <jinsunkim94@gmail.com>
2021-03-03 00:42:51 +0900

    javascript scroll_api

--- Javascript_mini/Coordinate/coordinate.js
+++ Javascript_mini/Coordinate/coordinate.js
@@ -2,7 +2,20 @@
 const abnormal = document.querySelector('.abnormal');
 abnormal.addEventListener('click', event =>{
     const abnormalRect = abnormal.getBoundingClientRect();
-        console.log(abnormalRect);
-        console.log(`client = ${event.clientX}, ${event.clientY}`);
-        console.log(`page = ${event.pageX}, ${event.pageY}`);
+    console.log(abnormalRect);
+    console.log(`client = ${event.clientX}, ${event.clientY}`);
+    console.log(`page = ${event.pageX}, ${event.pageY}`);
+});
+
+const scrollBy = document.querySelector('.scroll__by');
+const scrollTo = document.querySelector('.scroll__to');
+const scrollAbnormal = document.querySelector('.scroll__abnormal');
+scrollBy.addEventListener('click', event => {
+    window.scrollBy({top:100, left:0, behavior: 'smooth'});//100px씩 부드럽게
+});
+scrollTo.addEventListener('click', event => {
+    window.scrollTo(0, 100);//100px인 곳으로
+});
+scrollAbnormal.addEventListener('click', event => {
+    window.scrollTo(8, 800);
 });
