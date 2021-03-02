a944505eabddb5f90f2bd7dcf739cdeb4175d394
jinsunkimdev <jinsunkim94@gmail.com>
2021-03-02 21:36:28 +0900

    finish javascript_coordinate

--- Javascript_mini/Coordinate/coordinate.js
+++ Javascript_mini/Coordinate/coordinate.js
@@ -1,11 +1,8 @@
 'use strict'
 const abnormal = document.querySelector('.abnormal');
-const abnormalCoor = abnormal.getBoundingClientRect();
-
-function abnormalCoordinate(abnormal){
-    console.log(abnormalCoor);
-    console.log(`client = ${abnormal.clientHeight}, ${abnormal.clientWidth}`);
-    console.log(`page = ${window.pageXOffset}, ${window.pageYOffset}`);
-}
-
-abnormal.addEventListener('click', abnormalCoordinate(abnormal));
+abnormal.addEventListener('click', event =>{
+    const abnormalRect = abnormal.getBoundingClientRect();
+        console.log(abnormalRect);
+        console.log(`client = ${event.clientX}, ${event.clientY}`);
+        console.log(`page = ${event.pageX}, ${event.pageY}`);
+});
